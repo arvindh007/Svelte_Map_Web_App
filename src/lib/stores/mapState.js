36b2666603ed4a380c3f9690f557f194e10b15db
arvindh007 <>
2025-05-10 14:42:55 +0530

    second commit

--- src/lib/stores/mapState.js
+++ src/lib/stores/mapState.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 // Create a store to track if the state has been manually changed by user interaction
-export const userInteracted = writable(false);
+// export const userInteracted = writable(false);
 
 // Create a store to track if the map has been initialized
 export const mapInitialized = writable(false);
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -3,6 +3,12 @@
   import { showCapitals } from '$lib/stores/capitals';                          
   import Map from '../india-map/map.svelte';
   import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  
+  // Don't override URL parameters on mount
+  onMount(() => {
+    console.log("Map route mounted with params:", $page.url.searchParams.toString());
+  });
 </script>
 
 <Map/>
